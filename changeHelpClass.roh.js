c259c0f47c582c773c56fdd676eb898010a16703
holasoycael <holasoycael@gmail.com>
2019-12-20 01:58:09 -0300

    Add files via upload

---
+++ changeHelpClass.roh.js
@@ -0,0 +1,11 @@
+$(function(){$('header.nav a').click(function(){
+
+$('header.nav a').removeClass('help');
+$('article').removeClass('help');
+$(this).addClass('help');
+
+var id = $(this).data('id');
+var content = $('article').filter(function(){ return $(this).data("id") == id});content.addClass('help');
+	
+});
+});
