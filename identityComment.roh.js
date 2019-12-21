5374d613c58a55a57350b6c0ffb4e6aa93b4b6bd
holasoycael <holasoycael@gmail.com>
2019-12-20 21:14:54 -0300

    Update identityComment.roh.js

--- identityComment.roh.js
+++ identityComment.roh.js
@@ -11,7 +11,7 @@ function identityComment(cmmIMG){
 	var summary = imgtag + '';
 	div.innerHTML = summary;
 
-  var random= Math.floor(Math.random() * 9) + 0;
+  var random= Math.floor(Math.random() * 11) + 0;
   var bigSize = ["0px 0px",
                  "0px -37px",
                  "0px -74px",
@@ -20,7 +20,9 @@ function identityComment(cmmIMG){
                  "0px -185px",
                  "0px -222px",
                  "0px -259px",
-                 "0px -297px"];
+                 "0px -297px",
+                 "0px -333px",
+                 "0px -370px"];
   document.getElementById("identity"+cmmIMG).style.backgroundPosition=bigSize[random];
 
 }
