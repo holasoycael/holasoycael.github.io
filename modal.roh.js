a9c9c342e768db607e40f82843d7c245e1ee871a
holasoycael <holasoycael@gmail.com>
2019-12-22 01:07:13 -0300

    Update modal.roh.js

--- modal.roh.js
+++ modal.roh.js
@@ -1,25 +1,66 @@
 $(document).ready(function(){
 
-    $("a[rel=modal]").click( function(ev){
-        ev.preventDefault();
- 
-        var id = $(this).attr("href");
- 
-        var alturaTela = $(document).height();
-        var larguraTela = $(window).width();
-     
-        //colocando o fundo preto
-        $('#mascara').css({'width':larguraTela,'height':alturaTela});
-        $('#mascara').fadeIn(300); 
-        $('#mascara').fadeTo("slow",0.6);
- 
+		var jaFoiVistoNasUltimas24Horas = getCookie('nomeCookiePopUp');
+        var id = $('#GalleryThemes');
+        var hd = $('#mascara');
+
+	if(jaFoiVistoNasUltimas24Horas==null){
+		//Código para MOSTRAR o pop up
+		mostrarOuNaoPopUp('mostrarSim');
+	}else{
+		//Código para NÃO MOSTRAR pop up
+		mostrarOuNaoPopUp('mostrarNao');
+	}
+
+	function mostrarOuNaoPopUp(condicao){
+		if(condicao=='mostrarSim'){
+
+		//Código para MOSTRAR o pop up
+        $(id).show();
+        $(hd).fadeIn(0);
+		$(hd).fadeTo("slow",0.6);
+
+		definirNovaDataExpiracao();
+		}else if(condicao=='mostrarNao'){
+
+		//Código para NÃO MOSTRAR pop up
+        $(id).hide();
+        $(hd).hide();
+
+		}
+	}
+	function definirNovaDataExpiracao(){
+		var data=new Date();
+		var numeroDeDiasParaExpiracao=1;//1 dia
+		data.setTime(data.getTime()+(numeroDeDiasParaExpiracao*24*60*60*1000));
+		var expires="expires="+data.toUTCString();
+		var cookie='nomeCookiePopUp="Ainda não espirou";'+expires;
+		document.cookie=cookie;
+	}
+	function getCookie(nomeCookie){
+		var ca=document.cookie.split(';');
+		var name=nomeCookie+'=';
+		for(var i=0;i&lt;ca.length;i++){
+			var c=ca[i].trim();
+			if(c.indexOf(name)==0){
+				return c.substring(name.length,c.length);
+			}
+		}
+		return null;
+	}
         var left = ($(window).width() /2) - ( $(id).width() / 2 );
         var top = ($(window).height() / 2) - ( $(id).height() / 2 );
      
-        $(id).css({'top':top,'left':left});
-        $(id).show();   
+        $(id).css({'top':top,'left':left});   
+
+
+     $("a[rel=modal]").click( function(ev){
+        ev.preventDefault();
+        $(id).show();
+        $(hd).fadeIn(0);
+		$(hd).fadeTo("slow",0.6);
     });
- 
+
     $("#mascara").click( function(){
         $(this).hide();
         $(".window").hide();
