147b6640c31db40b1f0f35ccc50f3df44c9cb303
holasoycael <holasoycael@gmail.com>
2019-12-24 00:13:40 -0300

    Update scrolling.roh.js

--- scrolling.roh.js
+++ scrolling.roh.js
@@ -1,17 +1,6 @@
 $(document).ready(function(){
-
-	$('.down').on('click',function (e) {
-	e.preventDefault();
-
-      	$("body").removeClass("hide");
-	    var target = '.frame';
-	    var $target = $(target);
-
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top-0
-	    }, 900, 'swing', function () {
-	
-      	$("div.paging").addClass("none");
-	    });
-	});
+    $(".btnDown").click(function(){
+        $("div.paging").fadeOut();
+        $("body").removeClass("hide");
+    });
 });
