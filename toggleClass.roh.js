c65d8b9c43283c493bb5bf6a9d167ebf028b6f1a
holasoycael <holasoycael@gmail.com>
2019-12-27 16:56:17 -0300

    Update toggleClass.roh.js

--- toggleClass.roh.js
+++ toggleClass.roh.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 $('p.btn-menu a').click(function(){
     $('body').toggleClass('hide');
     $('.container').toggleClass('filte');
-    $('.frame, .ct-header').toggleClass('n-push hide');//transform menu
+    $('.frame, .ct-header').toggleClass('n-push');//transform menu
     $('.pushmenu').toggleClass('m-push');//transform menu
     $('#txtPesquisar, #inputSubmit').toggleClass('disabl');//block search
 });
