d05ec998f4480d710fcb3772fec02310c88c9dd5
holasoycael <holasoycael@gmail.com>
2020-05-01 18:07:24 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,30 @@
+(function (){
+
+const auth = firebase.auth();
+
+function signUp(){
+	var email = document.getElementById("email");
+	var senha = document.getElementById("password");
+	
+	const promise = auth.createUserWithEmailAndPassword(email.value, senha.value);
+	promise.catch(e => alert(e.message));
+	
+	alert("Signed In")
+}
+
+function signIn(){
+	var email = document.getElementById("email");
+	var senha = document.getElementById("password");
+	
+	const promise = auth.signInWithEmailAndPassword(email.value, senha.value);
+	promise.catch(e => alert(e.message));
+	
+	alert("Signed In")
+}
+
+function signOut(){
+	auth.signOut();
+	alert("Signed Out");
+}
+
+})();
