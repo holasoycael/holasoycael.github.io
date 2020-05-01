431bb18a9f726e51c06d49734cec5454c41b1a3b
holasoycael <holasoycael@gmail.com>
2020-05-01 06:50:52 -0300

    Add files via upload

---
+++ firebase-database.js
@@ -0,0 +1,14 @@
+(function () {
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyCx_f4AQg-9PViQ7dwfnV4Q-r9d84gQBb8",
+    authDomain: "user-4ae8a.firebaseapp.com",
+    databaseURL: "https://user-4ae8a.firebaseio.com",
+    projectId: "user-4ae8a",
+    storageBucket: "user-4ae8a.appspot.com",
+    messagingSenderId: "852542611513",
+    appId: "1:852542611513:web:9d1ac66ac666423fe12de8"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+})()
