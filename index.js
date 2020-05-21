a4d9871957956f6137ced67f3a0b499a560e40ae
holasoycael <holasoycael@gmail.com>
2020-05-21 09:18:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,103 +1,248 @@
-var fireBase = firebase.auth();
-var usersList = document.getElementById('usersList');
-var div = document.getElementById("contentHTML");
+window.onload = function(){
+var BTNmenu = document.querySelector('.BTNhref');
+var SWIPEc_ = document.querySelector('.SWIPEc_');
 
-var loginUser = '<h3>Login <span> ou <a href="#" onclick="signUp()">criar uma conta</a></span></h3><input type="email" placeholder="name@examplo.com" id="emailInput"></input><input type="password" placeholder="Senha" id="passwordInput"></input><button onclick="login()">Acessar conta</button></div>';
-var signedUser = '<div id="user_div" class="loggedin-div"><h3>Seja bem vindo(a)</h3><div id="usersList"><p id="user_para">Você está conectado no momento.</p></div><button onclick="logout()">Sair</button></div>';
-var signUpUser = '<h3>Criar uma conta <span>ou <a href="#" onclick="loginHome()">fazer login</a></span></h3><div id="divName"><input type="text" placeholder="Nome" id="nameInput"></input></div><div id="divSurname"><input type="text" placeholder="Sobrenome" id="lastNameInput"></input></div><div id="divAge"><input type="number" placeholder="Idade" id="ageInput"></input></div><div id="divEmail"><input type="email" placeholder="E-mail" id="emailInput"></input></div><div id="divPassword"><input type="password" placeholder="Senha" id="passwordInput"></input></div><div id="divPasswordConfirm"><input type="password" placeholder="Confirme sua senha" id="passwordConfirmInput"></input><input onclick="showPassword()" type="button" id="showPassword" value="Mostrar senha"></input></div><button onclick="sendPefil()" id="addButton">Criar uma conta</button>';
+BTNmenu.addEventListener('click', SWIPEopen);
+SWIPEc_.addEventListener('click', SWIPEclose);
 
-function sendPefil(){
-var nameInput = document.getElementById('nameInput');
-var lastNameInput = document.getElementById('lastNameInput');
-var ageInput = document.getElementById('ageInput');
-var emailInput = document.getElementById('emailInput');
-var passwordInput = document.getElementById('passwordInput');
-var passwordConfirmInput = document.getElementById('passwordConfirmInput');
-var eItem = emailInput.value;
-var pItem = passwordInput.value;
-var Result = "true";
+var BODYdoc = document.getElementsByTagName("BODY")[0];
+var PUSHmenu = document.querySelector('.PUSHmenu');
+var GUIDEmenu = document.querySelector('.GUIDEmenu');
+var SWIPEopen = document.getElementById('CONTENTframe');
 
-if(eItem != ""){
-fireBase.createUserWithEmailAndPassword(eItem, pItem).then(function (user) {
-	var user = fireBase.currentUser;
-	var userId = user.uid;
-	var nItem = nameInput.value;
-	var lItem = lastNameInput.value;
-	var aItem = ageInput.value;
-	var status = "Active";
-	firebase.database().ref('default/' + userId).set({
-	USERname: nItem,
-	USERnami: lItem,
-	USERage: aItem,
-	USERstt: status});
-	return Result;
-},function(error){
-	// Handle Errors here.
-	var errorCode = error.code;
-	var errorMessage = error.message;
-	return Result = "false";
-	// ...
-});
-	}else{
-var divName = document.getElementById("divName");
-var divSurname = document.getElementById("divSurname");
-var divAge = document.getElementById("divAge");
-var divEmail = document.getElementById("divEmail");
-var divPassword = document.getElementById("divPassword");
-//continuar por aqui...
-	alert("Erro!");	}
-} // function sendPefil();
+function HEADmenu(){
+var HEADmenu = document.querySelector('HEADER.HEADmenu');
+var HTMLoffset = document.querySelector('HTML');
+HEADmenu.style.width = HTMLoffset.offsetWidth + 'px';}
+setInterval(HEADmenu, 0);
 
-function showPassword() {
-var typeButton = document.getElementById("passwordInput").getAttribute("type");
-if(typeButton == "password"){
-	passwordInput.setAttribute("type", "text");
-	passwordConfirmInput.setAttribute("type", "text");}
-if(typeButton == "text"){
-	passwordInput.setAttribute("type", "password");
-	passwordConfirmInput.setAttribute("type", "password");}}
+function SWIPEopen(){
+BODYdoc.style.cssText = "overflow:hidden";
+PUSHmenu.style.cssText = "transform:translate3d(0,0,0)";
+GUIDEmenu.style.cssText = "left: 240px";
+SWIPEopen.classList.add('SWIPEopen');
 
-fireBase.onAuthStateChanged(function(user){
-if(user){
-	div.innerHTML = signedUser;
-	user = fireBase.currentUser;
-	if(user != null){
-	var email_id = user.email;
-	var user_id = user.uid;
-		
-let rootRef = firebase.database().ref();
-let primary = rootRef.child('default');
+var SWIPEcl = document.querySelector('.SWIPEopen');
+SWIPEcl.addEventListener('click', SWIPEclose);}
 
-var ajax = new XMLHttpRequest();
-ajax.open('GET', primary + '/' + user_id +'.json');
-ajax.responseType = 'json';
-ajax.send();
+function SWIPEclose(){
+BODYdoc.style.removeProperty('overflow');
+PUSHmenu.style.removeProperty('transform');
+GUIDEmenu.style.removeProperty('left');
+SWIPEopen.classList.remove('SWIPEopen');}
 
-ajax.addEventListener('readystatechange', function(){
-	if(ajax.readyState === 4 && ajax.status === 200){
-	var easyHTML = document.getElementById('user_para');
-	var resposta = ajax.response;
-	var USERage = resposta.USERage;
-	var USERname = resposta.USERname;
-	var USERnami = resposta.USERnami;
+//MOSTRAR MAIS E MOSTRAR MENOS DO FOOTER [OK]
+var _SPOTone = document.querySelector('._SPOTone');
+var FORSPOTone = document.getElementById('FORSPOTone');
+FORSPOTone.addEventListener('click', function(){
+if (_SPOTone.style.display === "block") {
+FORSPOTone.classList.remove('CROSSup');
+FORSPOTone.classList.add('CROSSdown');
+_SPOTone.style.display = "none";
+}else{
+FORSPOTone.classList.remove('CROSSdown');
+FORSPOTone.classList.add('CROSSup');
+_SPOTone.style.display = "block";}});
 
-	var cPanel = '<li>' +email_id+ '</li><li>' +user_id+ '</li><li>' + USERname + ' ' + USERnami + '</li><li>' +USERage+ '</li>';
+var _SPOTtwo = document.querySelector('._SPOTtwo');
+var FORSPOTtwo = document.getElementById('FORSPOTtwo');
+FORSPOTtwo.addEventListener('click', function(){
+if (_SPOTtwo.style.display === "block") {
+FORSPOTtwo.classList.remove('CROSSup');
+_SPOTtwo.style.display = "none";
+}else{
+FORSPOTtwo.classList.add('CROSSup');
+_SPOTtwo.style.display = "block";}});
 
-	easyHTML.innerHTML = cPanel;
-}});}
-}else{div.innerHTML = loginUser;}});
+var _SPOTtre = document.querySelector('._SPOTtre');
+var FORSPOTtre = document.getElementById('FORSPOTtre');
+FORSPOTtre.addEventListener('click', function(){
+FORSPOTtre.classList.remove('CROSSup');
+if (_SPOTtre.style.display === "block") {
+_SPOTtre.style.display = "none";
+}else{
+FORSPOTtre.classList.add('CROSSup');
+_SPOTtre.style.display = "block";}});
 
-function login(){
-var userEmail = document.getElementById("emailInput").value;
-var userPass = document.getElementById("passwordInput").value;
-fireBase.signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-// Handle Errors here
-var errorCode = error.code;
-var errorMessage = error.message;
-window.alert("Error: " + errorMessage);
-// ...
+//GRAVA O VALOR DA BUSCA [OK]
+document.querySelector('.SEARCHform').addEventListener('submit', SEARCHform);
+function SEARCHform(event) {
+	var SEARCHinput = document.querySelector('.SEARCHinput').value;
+if(SEARCHinput != ''){
+if(localStorage.LASTsearch != undefined){
+	var GETitem = localStorage.LASTsearch;
+	var GETitef = GETitem.substring(2, GETitem.length-2);
+	var OUTirm = '["' +GETitef+ '","' +SEARCHinput+ '"]';
+	var LASTes = GETitef.split('","');
+
+if(LASTes.length <= 3){
+	localStorage.setItem('LASTsearch', OUTirm);
+}else{
+	var SEARCtag = '["' +LASTes[1]+ '","' +LASTes[2]+ '","' +LASTes[3]+ '","' +SEARCHinput+ '"]';
+	localStorage.setItem('LASTsearch', SEARCtag);}}
+else{
+	var BAAHit = '["' +SEARCHinput+ '"]';
+	localStorage.setItem('LASTsearch', BAAHit);}}}
+
+//REGASTA E EXIBE O VALOR DA BUSCA [...]
+function LASTsearch(){
+if(localStorage.LASTsearch != undefined){
+var arr = localStorage.LASTsearch;
+var arr = arr.substring(2, arr.length-2);
+var arr = arr.split('","');
+
+if(arr[arr.length-1] != undefined){ var SEARCHtag_1 = '<a class="SEARCHtag" href="/search?q='+ arr[arr.length-1] +'&amp;max-results=9">'+ arr[arr.length-1] +'</a>' }
+else{ var SEARCHtag_1 = '' }
+if(arr[arr.length-2] != undefined){ var SEARCHtag_2 = '<a class="SEARCHtag" href="/search?q='+ arr[arr.length-2] +'&amp;max-results=9">'+ arr[arr.length-2] +'</a>' }
+else{ var SEARCHtag_2 = '' }
+if(arr[arr.length-3] != undefined){ var SEARCHtag_3 = '<a class="SEARCHtag" href="/search?q='+ arr[arr.length-3] +'&amp;max-results=9">'+ arr[arr.length-3] +'</a>' }
+else{ var SEARCHtag_3 = '' }
+if(arr[arr.length-4] != undefined){ var SEARCHtag_4 = '<a class="SEARCHtag" href="/search?q='+ arr[arr.length-4] +'&amp;max-results=9">'+ arr[arr.length-4] +'</a>' }
+else{ var SEARCHtag_4 = '' }
+
+var POPULARinst = document.querySelector('.POPULARinst');
+POPULARinst.innerHTML = '<i class="LASTsearch"></i>' +SEARCHtag_1+SEARCHtag_2+SEARCHtag_3+SEARCHtag_4;}}
+
+//RESGATA BOOKMARKS [OK]
+if(window.location.href.indexOf('favoritos') > -1){
+var BLOGinst = document.querySelector('.Blog');
+BLOGinst.classList.add('BOOKmark');
+
+if(localStorage.BOOKmark != undefined){
+var GETitem = localStorage.BOOKmark;
+var JSONparse = JSON.parse(GETitem);
+var OBJname = Object.getOwnPropertyNames(JSONparse);
+
+for(var i = 0; i < OBJname.length; i++){
+var pTITLE = JSONparse[OBJname[i]].POSTtitle;
+var pURL = JSONparse[OBJname[i]].POSTurl;
+var pPRICE = JSONparse[OBJname[i]].RELprice;
+var pIMG = JSONparse[OBJname[i]].RELimage;
+var pDATE = JSONparse[OBJname[i]].POSTdate;
+
+var CONTENTpage = '<div class="POSTbox BOOKmark" id="'+OBJname[i]+'"><div class="POSTinst"><section class="POSTup"><div class="POSTimg"><img class="THUMBnail" src="' +pIMG+ '"></img></div><div class="POSTspot"><i class="BTNdel" rel="'+OBJname[i]+'"></i></div></section><section class="POSTbt"><div class="POSTbt_inner"><h3 class="POSTtitle"><a class="POSTurl" href="'+pURL+'">'+pTITLE+'</a></h3><div class="POSTdate CALENDar">'+pDATE+'</div><div class="POSTBOXbt"><div class="POSTprice">'+pPRICE+'</div><div class="POSTbtn"><li><a class="BTNlive" href="#" target="_blank">Demo</a></li><li><a class="BTNinfo" href="#">Info</a></li></div></div></div></section></div></div>';
+BLOGinst.insertAdjacentHTML('beforeend', CONTENTpage)}
+if(OBJname.length > 1){
+var BOOKtitle = '<h3 class="BOOKtitle">Há ' +OBJname.length+ ' temas na sua lista!</h3>';}
+else{
+var BOOKtitle = '<h3 class="BOOKtitle">Só há ' +OBJname.length+ ' tema na sua lista!</h3>';}
+BLOGinst.insertAdjacentHTML('afterbegin', BOOKtitle)
+
+//REMOVER POST BOOKMARK [EM EDIÇÃO (...)]
+for(var i = 0; i < OBJname.length; i++){
+var BTNclick = document.querySelectorAll('.BTNdel');
+
+BTNclick[i].addEventListener('click', function(){
+var BTNrel = this.getAttribute('rel');
+var RECENTitem = localStorage.BOOKmark;
+var JSONparse = JSON.parse(RECENTitem);
+var OBJname = Object.getOwnPropertyNames(JSONparse);
+
+if(~OBJname.indexOf(BTNrel)){
+delete JSONparse[BTNrel]; // deleta o item selecionado
+var JSONstr = JSON.stringify(JSONparse); //converte em texto
+localStorage.setItem('BOOKmark', JSONstr)
+
+function DELTEpost(){
+if(OBJname.length > 2){
+var BOOKtitle = document.querySelector('.BOOKtitle');
+var BOOKtxt = `Há ${OBJname.length-1} temas na sua lista!`;
+	BOOKtitle.innerHTML = BOOKtxt;}
+else{
+var BOOKtitle = document.querySelector('.BOOKtitle');
+var BOOKtxt = `Só há ${OBJname.length-1} tema na sua lista!`;
+	BOOKtitle.innerHTML = BOOKtxt;}
+
+var THISpost = document.getElementById(BTNrel)
+THISpost.parentNode.removeChild(THISpost);}
+
+document.getElementById(BTNrel).style.cssText = "max-width:0;max-height:0;opacity:0";
+setTimeout(DELTEpost, 920)}
+
+function ZEROmark(){
+var BOOKtitle = document.querySelector('.BOOKtitle');
+BOOKtitle.parentNode.removeChild(BOOKtitle);
+document.querySelector('.EMPTYfav').style.cssText = "max-height:900px;opacity:1";}
+
+if(OBJname.length -1 == 0){
+	document.querySelector('.BOOKtitle').style.cssText = "opacity:0;max-height:0";
+	localStorage.removeItem('BOOKmark')
+var BLOGinst = document.querySelector('.Blog');
+var CONTENTpage = '<div class="EMPTYfav BOOKmark"><p>Nao há nenhum tema em seus favoritos... Não deixe-a solitaria. 😥</p><div class="SPOTfav"><div class="BOTfav"><div class="POSTbox EXEfav"><div class="POSTinst"><section class="POSTup excl"><div class="POSTimg"><img class="THUMBnail" src="https://2.bp.blogspot.com/-U1PEPnMPk2Q/Xr_BCoq7EAI/AAAAAAAAG5I/_LP4GUXyKTAH5WUi7H7zH2yfFFaBjTnYQCLcBGAsYHQ/s1600/village-de-traque.png"></div><div class="POSTspot EXEfav"><i class="BTNfav EXEfav"></i></div></section><section class="POSTbt"><div class="POSTbt_inner"><h3 class="POSTtitle"><span class="POSTurl EXEfav TITLEex"></span></h3><div class="POSTdate EXEfav DATEex"></div><div class="POSTBOXbt"><div class="POSTprice EXEfav PRICEex"></div><div class="POSTbtn EXEfav BUTTONSex"><li><span class="BTNlive EXEfav"></span></li><li><span class="BTNinfo EXEfav"></span></li></div></div></div></section></div></div></div><div class="BOTFAVright"><span class="MSGfav">Os temas adicionados a sua lista de favoritos duram até a limpeza dos dados do seu navegador ou excluí-los manualmente. Coloque novo temas a sua lista!</span><a href="#" class="BACKhome">Mostrar mais temas</a></div></div></div>';
+BLOGinst.insertAdjacentHTML('beforeend', CONTENTpage);
+setTimeout(ZEROmark, 700)}});}}
+else{
+var BLOGinst = document.querySelector('.Blog');
+var CONTENTpage = '<div class="EMPTYfav"><p>Você ainda não adicionou nenhum tema ao seus favoritos 🙄</p><div class="SPOTfav"><div class="BOTfav"><div class="POSTbox EXEfav"><div class="POSTinst"><section class="POSTup excl"><div class="POSTimg"><img class="THUMBnail" src="https://2.bp.blogspot.com/-U1PEPnMPk2Q/Xr_BCoq7EAI/AAAAAAAAG5I/_LP4GUXyKTAH5WUi7H7zH2yfFFaBjTnYQCLcBGAsYHQ/s1600/village-de-traque.png"></div><div class="POSTspot EXEfav"><i class="BTNfav EXEfav"></i></div></section><section class="POSTbt"><div class="POSTbt_inner"><h3 class="POSTtitle"><span class="POSTurl EXEfav TITLEex"></span></h3><div class="POSTdate EXEfav DATEex"></div><div class="POSTBOXbt"><div class="POSTprice EXEfav PRICEex"></div><div class="POSTbtn EXEfav BUTTONSex"><li><span class="BTNlive EXEfav"></span></li><li><span class="BTNinfo EXEfav"></span></li></div></div></div></section></div></div></div><div class="BOTFAVright"><span class="MSGfav">Clique no icone de “adicionar aos favoritos” e crie sua lista personalizada com todos os temas que tu mais gosta. Estamos felizes por ter você em nosso site.</span><a href="#" class="BACKhome">Mostrar todos os temas</a></div></div></div>';
+BLOGinst.insertAdjacentHTML('beforeend', CONTENTpage)}}
+
+//BOOOKMARK EXISTENTE
+if(window.location.href.indexOf('favoritos') > -1){
+if(localStorage.BOOKmark != undefined){
+	var GETitem = localStorage.BOOKmark;
+	var JSONparse = JSON.parse(GETitem);
+	var OBJname = Object.getOwnPropertyNames(JSONparse);
+for(var i = 0; i < OBJname.length; i++){
+	var ACTIVEpost = document.getElementById(OBJname[i]);
+ACTIVEpost.classList.add('ACTIVElist');}}}
+
+//ENVIAR BOOKMARK [OK]
+var BTNcount = document.querySelectorAll('.BTNfav').length;
+for(var i = 0; i < BTNcount; i++){
+var BTNclick = document.querySelectorAll('.BTNfav');
+
+BTNclick[i].addEventListener('click', function(){
+var BTNrel = this.getAttribute('rel');
+var POSTinst = document.getElementById(BTNrel);
+var RELtitle = POSTinst.querySelector('.POSTurl').innerText;
+var RELhref = POSTinst.querySelector('.POSTurl').href;
+var RELdate = POSTinst.querySelector('.POSTdate').innerText;
+var RELimage = POSTinst.querySelector('.THUMBnail').src;
+var RELprice = POSTinst.querySelector('.POSTprice').innerText;
+
+	var ACTIVEpost = document.getElementById(BTNrel);
+	ACTIVEpost.classList.add('ACTIVElist');
+
+if(localStorage.BOOKmark != undefined){
+var GETitem = localStorage.BOOKmark;
+if(!~GETitem.indexOf(BTNrel)){
+var MARKexist = GETitem.substring(1, GETitem.length-1);
+var BOOKmark = '{"' +BTNrel+ '":{"POSTtitle":"' +RELtitle+ '","POSTurl":"' +RELhref+ '","POSTdate":"' +RELdate+ '","RELimage":"' +RELimage+ '","RELprice":"' +RELprice+ '"},' +MARKexist+ '}';
+localStorage.setItem('BOOKmark', BOOKmark);
+}}else{
+var BOOKmark = '{"' +BTNrel+ '":{"POSTtitle":"' +RELtitle+ '","POSTurl":"' +RELhref+ '","POSTdate":"' +RELdate+ '","RELimage":"' +RELimage+ '","RELprice":"' +RELprice+ '"}}';
+localStorage.setItem('BOOKmark', BOOKmark);}
 });}
 
-function signUp(){ div.innerHTML = signUpUser; } //function ABRIR REGISTRO
-function loginHome(){ div.innerHTML = loginUser; } //function ABRIR LOGIN 
-function logout(){ fireBase.signOut(); } //function SAIR
+
+var SEARCHwhere = document.querySelector('.SEARCHwhere');
+var SEARCHbar = document.querySelector('.SEARCHbar');
+var HEADERnav = document.querySelector('.HEADERnav');
+var BACKmenu = document.querySelector('.BACKmenu');
+SEARCHwhere.addEventListener('click', function(){
+HEADERnav.style.cssText = "opacity:0;";
+SEARCHbar.style.cssText = "display:block";
+setTimeout(HEADmenuON, 100)
+});
+
+function HEADmenuON(){
+HEADERnav.style.cssText = "top: -56px;opacity:0;";
+SEARCHbar.style.cssText = "opacity:1;display:flex;";}
+
+BACKmenu.addEventListener('click', function(){
+SEARCHbar.style.cssText = "opacity:0;display:flex";
+setTimeout(HEADmenuOFF, 100)
+});
+
+function HEADmenuOFF(){
+SEARCHbar.removeAttribute("style");
+HEADERnav.style.cssText = "top:0;opacity:1";
+}
+
+if(window.location.href.indexOf('sobre') > -1){
+
+var BLOGinst = document.querySelector('.Blog');
+var POSTstatic = '<h3 class="STATICtitle">Sobre a empresa</h3><section class="BREADcrumb"><a href="#">Home</a><em class="CROSSicon"></em><span>Blogger Templates</span><em class="CROSSicon"></em><span class="CURRENTurl">Blog/Magazine</span></section><div class="POSTstatic">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus semper eget duis at tellus at. Viverra accumsan in nisl nisi. Lectus urna duis convallis convallis tellus id interdum velit laoreet. Mauris nunc congue nisi vitae suscipit tellus. Elementum integer enim neque volutpat ac tincidunt vitae. Donec adipiscing tristique risus nec. Posuere urna nec tincidunt praesent semper feugiat. Mattis enim ut tellus elementum sagittis vitae. Ut tellus elementum sagittis vitae et leo duis.</div>'
+BLOGinst.innerHTML = POSTstatic;
+
+}}
