268244aba8a82fe0198f50370fa4fee4c54427d8
holasoycael <holasoycael@gmail.com>
2020-06-09 17:21:22 -0300

    Update recentComments.js

--- recentComments.js
+++ recentComments.js
@@ -1,53 +1,58 @@
 function COMMENTdisplay(json){
+var urlNoAvatar = "data:image/svg+xml;base64,PHN2ZyBpZD0iQWdydXBhcl8xIiBkYXRhLW5hbWU9IkFncnVwYXIgMSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMjUwIiBoZWlnaHQ9IjI1MCIgdmlld0JveD0iMCAwIDI1MCAyNTAiPgogIDxkZWZzPgogICAgPHN0eWxlPgogICAgICAuY2xzLTEgewogICAgICAgIGZpbGw6ICNjNWM1YzU7CiAgICAgIH0KCiAgICAgIC5jbHMtMiwgLmNscy0zIHsKICAgICAgICBmaWxsOiAjZmZmOwogICAgICB9CgogICAgICAuY2xzLTMgewogICAgICAgIGZpbGwtcnVsZTogZXZlbm9kZDsKICAgICAgfQogICAgPC9zdHlsZT4KICA8L2RlZnM+CiAgPHJlY3QgaWQ9IlJldMOibmd1bG9fMSIgZGF0YS1uYW1lPSJSZXTDom5ndWxvIDEiIGNsYXNzPSJjbHMtMSIgd2lkdGg9IjI1MCIgaGVpZ2h0PSIyNTAiLz4KICA8Y2lyY2xlIGlkPSJFbGlwc2VfMSIgZGF0YS1uYW1lPSJFbGlwc2UgMSIgY2xhc3M9ImNscy0yIiBjeD0iMTI1IiBjeT0iMTAzIiByPSI1NyIvPgogIDxwYXRoIGlkPSJGb3JtYV8xIiBkYXRhLW5hbWU9IkZvcm1hIDEiIGNsYXNzPSJjbHMtMyIgZD0iTTMxLDI1MHM0LjE1Mi03Ny4yMzIsNjktOTljMS42NjQtLjA0NCw0Ny40MDYtMC4wMDcsNTAsMCwzNy41MTEsOC4yODUsNzAuMTUxLDYxLjM3NSw2OSw5OUMyMTUuNzY5LDI0OS45MDUsMzEsMjUwLDMxLDI1MFoiLz4KPC9zdmc+Cg==";
 var PIDcmmt = new Array();
+
 for(var i = 0; i < json.feed.entry.length; i++){
+	JSONit = json.feed.entry[i];
+	PIDcmmt += '"' +JSONit.id.$t.split('post-')[1]+ '"';
+    var ALTurl;
+    var ALTdat;
+    var ALTimg;
+	var ALTtit;
+	var ALTnam = JSONit.author[0].name.$t;
 
-    var entry = json.feed.entry[i];
-    var alturl;
-    var altdat;
-    var altimg;
-	var alttitle;
-	var authorName = entry.author[0].name.$t;
-	var altid = entry.id.$t;
+    for(var k = 0; k < JSONit.link.length; k++){
+	if(JSONit.link[k].rel == 'alternate'){
+ALTurl = JSONit.link[k].href;
+ALTtit = JSONit.title.$t;
+ALTdat = JSONit.gd$extendedProperty[1].value;
+ALTimg = JSONit.author[0].gd$image.src;
+break;}}
 
-PIDcmmt += '"' +altid.split('post-')[1]+ '"';
+    if (ALTimg == "https://img1.blogblog.com/img/blank.gif" != "") { ALTimg = urlNoAvatar; }
 
-console.log(altid.split('post-')[1]);
-
-
-
-
-    if(i == json.feed.entry.length) break;
-    for(var k = 0; k < entry.link.length; k++){
-      if(entry.link[k].rel == 'alternate'){
-alturl = entry.link[k].href;
-alttitle = entry.title.$t;
-altdat = entry.gd$extendedProperty[1].value;
-altimg  = entry.author[0].gd$image.src;
-        break;
-      }
-    }
-
-	var urlNoAvatar = "data:image/svg+xml;base64,PHN2ZyBpZD0iQWdydXBhcl8xIiBkYXRhLW5hbWU9IkFncnVwYXIgMSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMjUwIiBoZWlnaHQ9IjI1MCIgdmlld0JveD0iMCAwIDI1MCAyNTAiPgogIDxkZWZzPgogICAgPHN0eWxlPgogICAgICAuY2xzLTEgewogICAgICAgIGZpbGw6ICNjNWM1YzU7CiAgICAgIH0KCiAgICAgIC5jbHMtMiwgLmNscy0zIHsKICAgICAgICBmaWxsOiAjZmZmOwogICAgICB9CgogICAgICAuY2xzLTMgewogICAgICAgIGZpbGwtcnVsZTogZXZlbm9kZDsKICAgICAgfQogICAgPC9zdHlsZT4KICA8L2RlZnM+CiAgPHJlY3QgaWQ9IlJldMOibmd1bG9fMSIgZGF0YS1uYW1lPSJSZXTDom5ndWxvIDEiIGNsYXNzPSJjbHMtMSIgd2lkdGg9IjI1MCIgaGVpZ2h0PSIyNTAiLz4KICA8Y2lyY2xlIGlkPSJFbGlwc2VfMSIgZGF0YS1uYW1lPSJFbGlwc2UgMSIgY2xhc3M9ImNscy0yIiBjeD0iMTI1IiBjeT0iMTAzIiByPSI1NyIvPgogIDxwYXRoIGlkPSJGb3JtYV8xIiBkYXRhLW5hbWU9IkZvcm1hIDEiIGNsYXNzPSJjbHMtMyIgZD0iTTMxLDI1MHM0LjE1Mi03Ny4yMzIsNjktOTljMS42NjQtLjA0NCw0Ny40MDYtMC4wMDcsNTAsMCwzNy41MTEsOC4yODUsNzAuMTUxLDYxLjM3NSw2OSw5OUMyMTUuNzY5LDI0OS45MDUsMzEsMjUwLDMxLDI1MFoiLz4KPC9zdmc+Cg==";
-    if (altimg == "https://img1.blogblog.com/img/blank.gif" != "") { altimg = urlNoAvatar; }
-	
-
-	document.write('<div class="FLEXdiv"><a href="' +alturl.split('?')[0]+ '"><div class="FLOATdiv"><span class="IMGthub"><img src="' + altimg + '" width="42" height="42"/></span></div><div class="FLOATdiv"><section><b>' + authorName + '</b> fez um novo comentario: "'+alttitle+'"</section><section class="DATEcomment">' + altdat + '</section></div></a></div>');
-
-// console.log(Date().split('GMT')[0].split(' '))
-
-}
-
+	document.write('<div class="FLEXdiv"><a href="' +ALTurl.split('?')[0]+ '"><div class="FLOATdiv"><span class="IMGthub"><img src="' +ALTimg+ '" width="42" height="42"/></span></div><div class="FLOATdiv"><section><b>' + ALTnam + '</b> fez um novo comentario: "'+ALTtit+'"</section><section class="DATEcomment">' + ALTdat + '</section></div></a></div>');} //FIM DO LOOP
 
 	document.querySelector('.COMMENTdiv ').children[0].insertAdjacentHTML('beforeend', '<div class="SEEmore">Mostrar tudo</div>');
 	document.querySelector('.COMMENTdiv ').children[0].insertAdjacentHTML('afterbegin', '<div class="INFOcmmt">' +json.feed.entry.length+ ' comentários sendo exidos.</div>');
 
+if(Boolean(localStorage.NOTIFYit) !== true){
+	localStorage.setItem('CLOUDalt', '[' +PIDcmmt.replace(/("")/gi,'","')+ ']');}
 
-// var CLOUDalt = PIDcmmt.substring(1, PIDcmmt.length-1).split('""');
-var CLOUDalt = '[' +PIDcmmt.replace(/("")/gi,'","')+ ']';
+var ITEMapi = PIDcmmt.substring(1, PIDcmmt.length-1).split('""');
+var ITEMlocal = localStorage.CLOUDalt.substring(2, localStorage.CLOUDalt.length-2).replace(/(",")/gi, ',');
 
-console.log(PIDcmmt.substring(1, PIDcmmt.length-1).split('""'));
-	localStorage.setItem('CLOUDalt', CLOUDalt)
+if(Boolean(localStorage.NOTIFYit) !== true || ITEMapi.toString() !== ITEMlocal){
+	document.querySelector('.ALERToff').setAttribute('class', 'ALERTon');}
 
-// console.log('[' +PIDcmmt.replace(/("")/gi,'","')+ ']');
-}
+var NOTIFYit = document.querySelector('.NOTIFYit');
+var CLOSEout = document.querySelector('.CLOSEout');
+var SWIPEnav = document.getElementById('CONTENTframe');
+NOTIFYit.addEventListener('click', function(){
+
+if(Boolean(localStorage.NOTIFYit) !== true || ITEMapi.toString() !== ITEMlocal){
+	localStorage.setItem('NOTIFYit', 'true');
+	localStorage.setItem('CLOUDalt', '[' +PIDcmmt.replace(/("")/gi,'","')+ ']');
+	document.querySelector('.NOTIFYit').children[0].setAttribute('class', 'ALERToff');}
+
+	document.querySelector('.SIDEnav').classList.add('ACTIVEit');
+	SWIPEnav.classList.add('SWIPEnav');
+	document.body.style.cssText = "overflow:hidden";});
+CLOSEout.addEventListener('click', function(){
+	SWIPEnav.classList.remove('SWIPEnav');
+	document.querySelector('.SIDEnav').classList.remove('ACTIVEit');
+	document.body.removeAttribute('style');});
+SWIPEnav.addEventListener('click', function(){
+	this.classList.remove('SWIPEnav');
+	document.querySelector('.SIDEnav').classList.remove('ACTIVEit');
+	document.body.removeAttribute('style');});}
